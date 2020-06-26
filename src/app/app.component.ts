dc6becb7dbe9f02928cd0abdac79ab5e0e26ce13
Pierre Viara <viara@intechinfo.fr>
2020-06-26 17:17:37 +0200

    core(config): retrieve current version number

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { version } from '../../package.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+    public version: string = version;
+}
